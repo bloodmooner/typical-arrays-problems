320c732e2b16702bb848faf30368f9f0d88afc39
Sergey Goncharuk <fleshed914@gmail.com>
2021-08-16 10:34:51 +0300

    add min(), max(), avg()

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array === undefined || array.length == 0) return 0;
+
+    let minElem = array[0];
+
+    for(let i =0; i < array.length; i++) {
+        if(minElem < array[i]) {
+            minElem = minElem;
+        } else {
+            minElem = array[i];
+        }
+    }
+    return minElem;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === undefined || array.length == 0) return 0;
+
+    let maxElem = array[0];
+
+    for(let i =0; i < array.length; i++) {
+        if(maxElem > array[i]) {
+            maxElem = maxElem;
+        } else {
+            maxElem = array[i];
+        }
+    }
+    return maxElem;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === undefined || array.length == 0) return 0;
+
+    let sumElems = 0;
+
+    for(let i =0; i < array.length; i++) {
+        sumElems = sumElems + array[i];
+    }
+    return sumElems / array.length;
 }
